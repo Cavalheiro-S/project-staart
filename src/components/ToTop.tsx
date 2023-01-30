6e0f509d7790220cee5ac125a06f24a0b5e0fc72
Lucas Cavalheiro <luccribeiro53@gmail.com>
2023-01-30 18:16:48 -0300

    fix: adjust size of 'ToTop' component

--- src/components/ToTop.tsx
+++ src/components/ToTop.tsx
@@ -5,8 +5,8 @@ import { useGetScroll } from "../hooks/useGetScroll"
 export const ToTop = () => {
     const { scrollY } = useGetScroll();
     return scrollY > 0 ? (
-        <div className="fixed bottom-4 right-4 z-10">
-            <a href="#header" className="bg-primary rounded-full p-2">
+        <div className="fixed bottom-10 right-4 z-10">
+            <a href="#header" className="bg-primary rounded-full p-4">
                 <ArrowUpwardOutlined className="text-white" />
             </a>
         </div>
