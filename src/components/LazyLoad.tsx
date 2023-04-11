ba9c008dd7fabf0771851170cc060e65e1567b50
Lucas Cavalheiro <luccribeiro53@gmail.com>
2023-04-11 07:37:34 -0300

    fix: add enviromment variable to path of pages

--- src/components/LazyLoad.tsx
+++ src/components/LazyLoad.tsx
@@ -2,7 +2,7 @@ import React, { lazy } from 'react'
 
 export const LazyLoad = (path: string, name: string) => {
     return lazy(() => (
-        import(`../pages/${path}`).then(module => {
+        import(import.meta.env.VITE_PATH_PAGES + path).then(module => {
             return { default: module[name] }
         })
     ))
